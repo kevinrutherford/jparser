8d7f2ac32270b01f1c6beff59b2cd76f8e02f499
Kevin Rutherford <kevin@rutherford-software.com>
2014-07-23 12:35:36 +0100

    Removed more stuff

--- src/main/java/javancss/Javancss.java
+++ src/main/java/javancss/Javancss.java
@@ -34,44 +34,16 @@ import javancss.parser.JavaParserTokenManager;
 import javancss.parser.TokenMgrError;
 import javancss.parser.debug.JavaParserDebug;
 
-public class Javancss implements Exitable
+public class Javancss
 {
-    private static final String S_INIT__FILE_CONTENT =
-        "[Init]\n" +
-        "Author=Chr. Clemens Lee\n" +
-        "\n" +
-        "[Help]\n"+
-        "; Please do not edit the Help section\n"+
-        "HelpUsage=@srcfiles.txt | *.java | <stdin>\n" +
-        "Options=ncss,package,object,function,all,out,recursive,encoding\n" +
-        "ncss=b,o,Counts the program NCSS (default).\n" +
-        "package=b,o,Assembles a statistic on package level.\n" +
-        "object=b,o,Counts the object NCSS.\n" +
-        "function=b,o,Counts the function NCSS.\n" +
-        "all=b,o,The same as '-function -object -package'.\n" +
-        "out=s,o,Output file name. By default output goes to standard out.\n"+
-        "recursive=b,o,Recurse to subdirs.\n" +
-        "encoding=s,o,Encoding used while reading source files (default: platform encoding).\n" +
-        "\n" +
-        "[Colors]\n" +
-        "UseSystemColors=true\n";
-    
-    private Init _pInit = null;
-    private boolean _bExit = false;
-    private List<File> _vJavaSourceFiles = null;
     private JavaParserInterface parser = null;
     private FileMetrics _fileMetrics = new FileMetrics();
 
-    public Javancss(String[] asArgs_) throws IOException
+    public Javancss(String[] argv) throws IOException
     {
-        _pInit = new Init( this, asArgs_, Main.S_RCS_HEADER, S_INIT__FILE_CONTENT );
-        if ( _bExit )
-            return;
-        Map<String, String> htOptions = _pInit.getOptions();
-        _vJavaSourceFiles = findFiles( _pInit.getArguments());
         try {
-            _measureRoot();
-        } catch ( Throwable pThrowable ) {
+            _measureRoot(argv[0]);
+        } catch (Throwable pThrowable) {
             pThrowable.printStackTrace(System.err);
             return;
         }
@@ -84,43 +56,17 @@ public class Javancss implements Exitable
         }
     }
 
-    private void _measureRoot() throws Exception
+    private void _measureRoot(String path) throws Exception
     {
-        for (File path : _vJavaSourceFiles) {
-            Reader reader = newReader(path);
-            parser = new JavaParser(reader);
-            parser.parse();
-            parser.collectFileMetrics(_fileMetrics);
-            _fileMetrics.filename = path.getPath();
-        }
+        Reader reader = newReader(new File(path));
+        parser = new JavaParser(reader);
+        parser.parse();
+        parser.collectFileMetrics(_fileMetrics);
+        _fileMetrics.filename = path;
     }
 
-    private List<File> findFiles( List<String> filenames) throws IOException {
-        if ( filenames.size() == 0 )
-            return null;
-
-        Set<String> _processedAtFiles = new HashSet<String>();
-        List<File> newFiles = new ArrayList<File>();
-        for ( String filename : filenames )
-        {
-            filename = FileUtil.normalizeFileName( filename );
-            File file = new File( filename );
-            newFiles.add( file );
-        }
-        return newFiles;
-    }
-
-    public void setExit()
-    {
-        _bExit = true;
-    }
-
-    private Reader newReader( InputStream stream ) {
-        return new InputStreamReader(stream);
-    }
-
-    private Reader newReader( File file ) throws FileNotFoundException {
-        return newReader( new FileInputStream( file ) );
+    private Reader newReader(File file) throws FileNotFoundException {
+        return new InputStreamReader((new FileInputStream(file)));
     }
 
     private void printFileStats(Writer w) throws IOException {
